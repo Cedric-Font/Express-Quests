7719083f04f2c21dcfcb10f65e5006238026c4b6
Cedric-Font <font.cedric38@gmail.com>
2023-11-27 14:40:34 +0100

    finished

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+const port = process.env.APP_PORT;
+
 
 const movieControllers = require("./controllers/movieControllers");
-
+const usersControlers = require("./controllers/usersControlers");
+console.log(port)
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", usersControlers.getUsers);
+app.get("/api/users/:id", usersControlers.getUsersById);
 
 module.exports = app;
